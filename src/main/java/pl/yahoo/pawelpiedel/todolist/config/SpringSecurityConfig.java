a847054dc5150a491269d13dca696d547421df7a
ppiedel <zespolok666>
2018-01-06 19:30:56 +0100

    Logout successfully implemented.

--- src/main/java/pl/yahoo/pawelpiedel/todolist/config/SpringSecurityConfig.java
+++ src/main/java/pl/yahoo/pawelpiedel/todolist/config/SpringSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +35,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/console").permitAll()
                 .antMatchers("/login/**").permitAll()
                 .antMatchers("/register/**").permitAll()
+                .antMatchers("/logout/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -47,7 +49,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().exceptionHandling().accessDeniedPage("/access-denied")
                 .and()
                 .logout()
-                .permitAll();
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .deleteCookies("remove")
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/logout/succesfull");
     }
 
     @Override
--- src/main/java/pl/yahoo/pawelpiedel/todolist/controller/LoginController.java
+++ src/main/java/pl/yahoo/pawelpiedel/todolist/controller/LoginController.java
@@ -1,28 +1,37 @@
 package pl.yahoo.pawelpiedel.todolist.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import pl.yahoo.pawelpiedel.todolist.dto.UserDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    /*@RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model) {
         model.addAttribute("user", new UserDto());
         return "login";
-    }*/
+    }
 
-    @RequestMapping("/login/error")
+    @RequestMapping(value = "/login/error", method = RequestMethod.GET)
     public String loginError(Model model) {
         model.addAttribute("user", new UserDto());
         model.addAttribute("error", true);
         return "login";
     }
 
-    @RequestMapping("/login/success")
-    public String loginSuccess(Model model) {
+    @RequestMapping(value = "/login/success", method = RequestMethod.GET)
+    public String loginSuccess(HttpServletRequest request, HttpServletResponse response, Model model) {
+        System.out.println("Login success");
+
         model.addAttribute("user", new UserDto());
         model.addAttribute("success", true);
 
@@ -30,7 +39,18 @@ public class LoginController {
     }
 
 
-    @RequestMapping("/access-denied")
+    @RequestMapping(value = "/logout/succesfull", method = RequestMethod.GET)
+    public String logoutSuccesfull(Model model) {
+        System.out.println("Logout success");
+        logger.info("Jestem w logout success");
+
+        model.addAttribute("user", new UserDto());
+        model.addAttribute("logout", true);
+        return "login";
+    }
+
+
+    @RequestMapping(value = "/access-denied", method = RequestMethod.GET)
     public String accessDenied() throws Exception {
         throw new Exception("Nie masz uprawnień aby wyświetlć tą stronę. Czy wpisany link jest poprawny?");
     }
